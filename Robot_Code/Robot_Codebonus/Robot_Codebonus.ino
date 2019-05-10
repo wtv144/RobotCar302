7abb39a25a53d55a89f9b59fd2f22bbeb020ca2b
ziyanp <43937209+ziyanp@users.noreply.github.com>
2019-05-10 12:17:35 -0500

    new

--- Robot_Code/Robot_Codebonus/Robot_Codebonus.ino
+++ Robot_Code/Robot_Codebonus/Robot_Codebonus.ino
@@ -1,4 +1,4 @@
-//Constants new
+//Constants
 const int pResistor1 = A0; // Photoresistor at Arduino analog pin A0  LEFT
 const int pResistor2 = A1;// Led pin at Arduino pin 9 RIGHT
 
@@ -6,6 +6,8 @@ const int pResistor2 = A1;// Led pin at Arduino pin 9 RIGHT
 
 
 const int dSensor1 = A2; // Distance sensors
+const int ldSensor = A3;
+const int rdSensor =  A4;
 
 
 const int leftforward = 2;      //Motor control pins
@@ -13,10 +15,14 @@ const int rightforward = 3;
 
 
 const int obsDistance = 480;    //constants used to evaluate
-const int rblackVal = 260;
+const int obsDistanceb = 500;
+const int rblackVal = 350;
             //800~~~ in dark environment (CLobby), 200-300 in bright environment (makerstudio 2nd flor)
-const int lblackVal = 260 ;
+const int lblackVal = 350 ;
 const int light = 13;
+
+const int redMin = 260;
+const int redMax = 325;
 
 
 
@@ -25,6 +31,8 @@ int pvalue1;          //Left resistor
 int pvalue2; //middle resistor
 
 int dvalue1;
+int dvalue2;
+int dvalue3;
 
 
 void setup(){
@@ -48,19 +56,26 @@ void loop(){
   pvalue2 = analogRead(pResistor2);
   
   dvalue1 = analogRead(dSensor1);
-
+  dvalue2 = analogRead(ldSensor);
+  dvalue3 = analogRead(rdSensor);
+ /*
+Serial.print(pvalue1);
+Serial.print("\n");
+delay(1000);
+Serial.print(pvalue2);
+Serial.print("\n");
+delay(1000);
 
 
 //You can change value "25"
 
-  
+  */
     
-if( dvalue1 < obsDistance) {
+if( dvalue1 < obsDistance && !isRed(pvalue1, pvalue2)) {
   Serial.write("NIF \n");
 
   if( pvalue2 > rblackVal && pvalue1 < lblackVal){
     Serial.write("RT \n");
-    //Serial.print(pvalue2);
     right();
     
   }
@@ -68,10 +83,17 @@ if( dvalue1 < obsDistance) {
     left();
     Serial.write("LT \n");
   }
+  else if (dvalue2 > obsDistanceb){
+    right();
+    Serial.write("RT");
+  }
+  else if (dvalue3 > obsDistanceb){
+    left();
+    Serial.write("LT");
+  }
   else{
     goStraight();
     Serial.write("S \n");
-    //Serial.print(pvalue2);
   }
   
   
@@ -80,7 +102,7 @@ if( dvalue1 < obsDistance) {
 else{
   stopMotor();
   Serial.write("SIF \n");
-  Serial.print(dvalue1);
+  
 }
 delay(200);
  
@@ -123,3 +145,8 @@ void stopMotor(){
   
   
 }
+
+bool isRed(int value1, int value2){
+  return(value1>redMin && value1<redMax) || (value2>redMin && value2<redMax);
+}
+  
