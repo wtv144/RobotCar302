b3365e9eee6b5a894cab95a14645bc3b4704187f
wtv144 <warren.vu@gmail.com>
2019-05-10 16:40:10 -0500

    final code

--- Robot_Code_Bonus.ino/Robot_Code_Bonus.ino.ino
+++ Robot_Code_Bonus.ino/Robot_Code_Bonus.ino.ino
@@ -1,34 +1,36 @@
 
 //Constants
-const int pResistor1 = A0; // Photoresistor at Arduino analog pin A0
-const int pResistor2 = A1;// Led pin at Arduino pin 9
-const int pResistor3 = A2;
+const int pResistor1 = A0; // Photoresistor at Arduino analog pin A0  LEFT
+const int pResistor2 = A1;// Led pin at Arduino pin 9 RIGHT
+const int pResistor3 = A5;
 
 
+const int dSensor1 = A2; // Distance sensors
 
-const int dSensor1 = A3; // Distance sensors
-const int dSensor2 = A4;
-const int dSensor3 = A5;
+const int ldSensor = A3;
+const int rdSensor = A4;
 
 const int leftforward = 2;      //Motor control pins
-const int leftback = 3;
-const int rightforward = 4;
-const int rightback =5;
+const int rightforward = 3;
 
-const int obsDistance = 200;    //constants used to evaluate
-const int rblackVal = 500;
-const int mblackVal = 550;       //800~~~ in dark environment (CLobby), 200-300 in bright environment (makerstudio 2nd flor)
-const int lblackVal = 500 ;
+
+const int obsDistance = 700;    //constants used to evaluate
+const int obsDistanceb =450 ;
+const int rblackVal = 300;
+            //800~~~ in dark environment (CLobby), 200-300 in bright environment (makerstudio 2nd flor)
+const int lblackVal = 300 ;
 const int light = 13;
 
+const int redMin = 295;
+const int redMax = 550;
 
 
 //Variables
 int pvalue1;          //Left resistor
 int pvalue2; //middle resistor
-int pvalue3;
+int pvalue3; //red resistor
 int dvalue1;
-int dvalue2;
+int dvalue2; 
 int dvalue3;
 
 void setup(){
@@ -37,8 +39,8 @@ void setup(){
  pinMode(pResistor2, INPUT);
  pinMode(pResistor3, INPUT);
  pinMode(dSensor1, INPUT);
- pinMode(dSensor2, INPUT);
- pinMode(dSensor3, INPUT);
+ pinMode(ldSensor, INPUT);
+ pinMode(rdSensor, INPUT);
  pinMode(leftforward, OUTPUT);
  pinMode(rightforward, OUTPUT);
  Serial.begin(9600);
@@ -48,121 +50,121 @@ void setup(){
 }
 
 void loop(){
- digitalWrite(leftforward, HIGH);
- digitalWrite(rightforward, HIGH);
+ 
+ 
  pvalue1 = analogRead(pResistor1);
   pvalue2 = analogRead(pResistor2);
   pvalue3 = analogRead(pResistor3);
+  
   dvalue1 = analogRead(dSensor1);
-  dvalue2 = analogRead(dSensor2);
-  dvalue3 = analogRead(dSensor3);
-  //You can change value "25"
+
+  dvalue2 = analogRead(ldSensor);
+  dvalue3 = analogRead(rdSensor);
+
+//You can change value "25"
+String myString = String(pvalue3);
+String fsensor = String(dvalue1);
+String rsensor = String(pvalue2);
+String lsensor = String(pvalue1);
+String ld = String(dvalue2);
+String rd = String(dvalue3);
+
+      
+  Serial.println("Red sensor: " +myString);
+  Serial.println("Front: " + fsensor);
+  Serial.println("Right Photoresistor: "+ rsensor);
+  Serial.println("Left Photoresistor: " +lsensor);
+  Serial.println("LDS: " + ld);
+  Serial.println("RDS: " +rd);
+
+if( dvalue1 < obsDistance && !isRed(pvalue3) ) {
   
-  /*if(pvalue1> lblackVal){
-    digitalWrite(light, HIGH);
-    Serial.write("B \n");
-    delay(500);
-    
-  }
-  else{
-    digitalWrite(light, LOW);
-    Serial.write("NB \n");
-    delay(500);
-  }
+  Serial.write("NIF \n");
+  /*Serial.println(myString);
+  Serial.println(fsensor);
+  Serial.println(rsensor);
+  Serial.println(lsensor);
   */
-
-if(dvalue1 < obsDistance ) 
-{
-  Serial.write("NIF\n");
-  digitalWrite(light, HIGH);
- 
   
- if(pvalue2 > mblackVal)
- { 
-    Serial.write("M \n");
+
+  if( pvalue2 > rblackVal ){
+    Serial.write("RT \n");
+    right();
     
-    
-  if(pvalue3 > rblackVal )
-  {
-    Serial.write("R \n");
-    //right();
-    delay(500);
   }
-  else if (pvalue1 >lblackVal)
-    {
-      Serial.write("L \n");
-    // left();
-    }
-        else
-        {
-          
-        Serial.write("S \n");
-       // goStraight();
-        }
+  else if (pvalue1 > rblackVal  ){
+    left();
+    Serial.write("LT \n");
+  }
+  else if (dvalue2> obsDistanceb){
+    right();
+    Serial.write("RTDS");
+   
+  }
+  else if (dvalue3 >obsDistanceb){
+    left();
+    Serial.write("LTDS");
+  }
   
- }
-
   else{
-  if(pvalue1 > lblackVal)
-      {
-       Serial.write("L2\n");
-       //left();
-      }
-  if(pvalue3 > rblackVal)
-    {
-    Serial.write("R2\n");
-    //right();
-    }
- 
-        }
-}
-
-else {
-  digitalWrite(light, LOW);
- // stopMotor();
-  Serial.write("NN \n");
+    goStraight();
+    Serial.write("S \n");
+  }
   
- 
   
 }
 
- 
+else{
+  stopMotor();
+  Serial.write("SIF \n");
+  if(isRed(pvalue3)){
+    Serial.write("STOP");
+  }
 }
+
+delay(250);
+}
+
 void goStraight(){
   
-    for(int i = 0; i <500; i++){
+    for(int i = 0; i <300; i++){
   digitalWrite(leftforward, HIGH);
   digitalWrite (rightforward, HIGH);
-  delayMicroseconds(500);
+  delayMicroseconds(325);
   digitalWrite(leftforward,LOW);
   digitalWrite(rightforward, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(675);
     }
 
 }
 void left() {
- for(int i = 0; i <500; i++){
+ for(int i = 0; i <300; i++){
  digitalWrite(leftforward, LOW);
   digitalWrite (rightforward, HIGH);
-  delayMicroseconds(250);
+  delayMicroseconds(400);
   digitalWrite(leftforward,LOW);
   digitalWrite(rightforward, LOW);
-  delayMicroseconds(750);
+  delayMicroseconds(600);
  }
 }
 void right() {
-  for(int i = 0; i <500; i++){
+  for(int i = 0; i <200; i++){
   digitalWrite(leftforward, HIGH);
   digitalWrite (rightforward, LOW);
-  delayMicroseconds(250);
+  delayMicroseconds(300);
   digitalWrite(leftforward,LOW);
   digitalWrite(rightforward, LOW);
-  delayMicroseconds(750);
+  delayMicroseconds(700);
   }
 }
 void stopMotor(){
   digitalWrite(leftforward,LOW);
   digitalWrite(rightforward, LOW);
   
+  
+}
+
+bool isRed(int p3val){
+  return (p3val > redMin && p3val < redMax) ;
   
 }
